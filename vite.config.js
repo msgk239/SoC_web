3e0430fa707988a730e24c75a27c815502ceca9a
msgk <zxr935867802@outlook.com>
2025-03-19 20:03:10 +0800

    0.0.6

--- vite.config.js
+++ vite.config.js
@@ -11,11 +11,6 @@ export default defineConfig({
       },
     },
   },
-  server: {
-    fs: {
-      strict: false // 允许访问项目根目录
-    }
-  },
   publicDir: 'public',  // 明确指定 public 目录
   optimizeDeps: {
     exclude: ['godot'] // 排除 godot 相关文件的优化
